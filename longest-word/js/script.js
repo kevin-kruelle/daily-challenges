20dfedd6cd303b3aa5e59d3300ba31dcbbdbb122
Kevin Kruelle <kkruelle2@gmail.com>
2021-03-15 15:31:23 -0400

    update

--- longest-word/js/script.js
+++ longest-word/js/script.js
@@ -1,4 +1,16 @@
-function LongestWord(sen) {
+let sentence = 'This is a string and I have to find the longest word in this string.'
+
+function longestWord(sen) {
   // code goes here
-  return sen;
+  let sentenceArr = sen.split(' ').join(',').split('.').join(',').split(',');
+  console.log(sentenceArr)
+  let word = '';
+  for (let i = 0; i < sentenceArr.length; i++) {
+    if (sentenceArr[i].length > word.length) {
+      word = sentenceArr[i];
+    }
+  }
+  return word;
 }
+
+console.log(longestWord(sentence));
